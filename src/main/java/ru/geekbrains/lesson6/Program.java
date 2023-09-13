e71bdbba840e7a679e105fd213057c952c48fc01
Roman <toolatet@gmail.com>
2023-09-13 16:43:51 +0700

    init

---
+++ src/main/java/ru/geekbrains/lesson6/Program.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.lesson6;
+
+
+import ru.geekbrains.lesson6.database.NotesDatabase;
+import ru.geekbrains.lesson6.notes.core.application.ConcreteNoteEditor;
+import ru.geekbrains.lesson6.notes.core.domain.Note;
+import ru.geekbrains.lesson6.notes.infrastructure.persistance.NotesDbContext;
+import ru.geekbrains.lesson6.notes.presentation.queries.controllers.NotesController;
+import ru.geekbrains.lesson6.notes.presentation.queries.views.NotesConsolePresenter;
+
+import java.util.Date;
+
+public class Program {
+
+    public static void main(String[] args) {
+        NotesController controller = new NotesController(
+                new ConcreteNoteEditor(new NotesDbContext(new NotesDatabase()), new NotesConsolePresenter()));
+        controller.routeGetAll();
+        controller.routeAddNote(new Note(1,1,"sds", "dsd", new Date()));
+//        controller.routeGetAll();
+    }
+
+}
